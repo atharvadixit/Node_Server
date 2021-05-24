b21c59b1b923739b89be5a818aa01e529ec6931a
atharvadixit <atharvadixit15@gmail.com>
2021-05-24 17:13:41 +0530

    Added routes to fetch user data

--- app.js
+++ app.js
@@ -3,20 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testAPIRouter = require("./routes/testAPI");
 
 var app = express();
-
-const swStats = require('swagger-stats');
-app.use(swStats.getMiddleware());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/getUsersData", testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/testAPI.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+'use strict';
+
+router.get('/', function(req, res, next) {
+
+    const fs = require('fs');
+
+    let rawdata = fs.readFileSync('sample.json');
+    let data = JSON.parse(rawdata);
+    console.log(data);
+
+    res.send(data);
+});
+
+module.exports = router;
