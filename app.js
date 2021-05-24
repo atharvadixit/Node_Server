a556f38374b5ddf38ec166355fbb382d30b9e4ff
atharvadixit <atharvadixit15@gmail.com>
2021-05-24 20:22:33 +0530

    Added swagger-stats

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require("./routes/testAPI");
 
 var app = express();
+
+const swStats = require('swagger-stats');
+app.use(swStats.getMiddleware());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
