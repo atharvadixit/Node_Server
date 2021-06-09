a96e5cb0e2d8eea28daedbd826d4ace0958861b5
atharvadixit <atharvadixit15@gmail.com>
2021-06-09 15:41:04 +0530

    Added API for returning company data

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/getUsersData", testAPIRouter);
+app.use("/getCompanyData", testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
